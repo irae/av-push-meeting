9904c95ef576e5652c0d2eb880c2c7a7b23e4e54
Iraê <irae@irae.pro.br>
2012-07-03 16:27:44 -0300

    Refactored to push button

--- server.js
+++ server.js
@@ -1,21 +1,52 @@
+/*jslint nomen:true */
+/*globals __dirname */
+"use strict";
 var express = require('express');
 var sockjs  = require('sockjs');
 
 // 1. Echo sockjs server
 var sockjs_opts = {sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"};
 
-var sockjs_echo = sockjs.createServer(sockjs_opts);
-sockjs_echo.on('connection', function(conn) {
-    conn.on('data', function(message) {
-        conn.write(message);
+var sockjs_server = sockjs.createServer(sockjs_opts);
+
+var connections = [];
+
+var updateStatus = function () {
+    var talk = false;
+    connections.forEach(function (conn) {
+        talk = talk || conn.talk;
     });
+    connections.forEach(function (conn) {
+        conn.write(talk ? 'on' : 'off');
+    });
+};
+
+sockjs_server.on('connection', function (conn) {
+    conn.talk = false;
+
+    conn.on('data', function (message) {
+        if (message === 'on') {
+            conn.talk = true;
+        } else {
+            conn.talk = false;
+        }
+        updateStatus();
+    });
+
+    conn.on('close', function () {
+        conn.talk = false;
+        connections.splice(connections.indexOf(conn), 1);
+        updateStatus();
+    });
+
+    connections.push(conn);
 });
 
 // 2. Express server
 var app = express.createServer();
-sockjs_echo.installHandlers(app, {prefix:'/echo'});
+sockjs_server.installHandlers(app, {prefix: '/push_to_talk'});
 
-console.log(' [*] Listening on 0.0.0.0:9999' );
+console.log(' [*] Listening on 0.0.0.0:9999');
 app.listen(9999, '0.0.0.0');
 
 app.get('/', function (req, res) {
